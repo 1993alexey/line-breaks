0a3bc06c585297b92a5eb1251d675370040aa973
Alex Shnyrov <alex.shnyrov@legrand.us>
2021-04-23 13:50:46 -0600

    stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+/* tslint:disable */
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,59 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'line-breaks';
+  userInput = ''
+  inputLimit = 10
+
+  textAreaChanged(e): void {
+    const val = e.value;
+    const lines = this.getLines(val)
+
+    let parsedLines = []
+    for (let line of lines)
+      if (line.length > this.inputLimit)
+        parsedLines = [...parsedLines, ...this.breakLine(line)]
+      else
+        parsedLines.push(line)
+
+    this.userInput = this.stitchLines(parsedLines)
+  }
+
+  stitchLines(lines: string[]): string {
+    const result = lines.reduce((acc, val) => {
+      return acc += val + '\n'
+    }, '')
+
+    if (result.length >= 2)
+      return result.substr(0, result.length - 1)
+
+    return ''
+  }
+
+  getLines(text): string[] {
+    return text.split('\n')
+  }
+
+  breakLine(line: string): string[] {
+    if (!line)
+      return []
+
+    if (line.length <= this.inputLimit)
+      return [line]
+
+    let lineTemp = line;
+    const lines = []
+    while(lineTemp) {
+      let subLine = lineTemp.substr(0, this.inputLimit)
+      const lineEndIndex = subLine.lastIndexOf(' ')
+      if (lineEndIndex !== -1)
+        subLine = subLine.substr(0, lineEndIndex)
+
+      lines.push(subLine)
+      lineTemp = lineTemp.substr(subLine.length).trim()
+    }
+
+    return lines
+  }
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {MatInputModule} from '@angular/material/input';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatInputModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
